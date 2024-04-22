6a0aeee3834ec0d650e4b91c6486cb42e72f884a
burnedAtoms <46892591+burnedAtoms@users.noreply.github.com>
2024-04-23 02:08:33 +0800

    Fix: CSS styling changes to components

--- src/Regions.jsx
+++ src/Regions.jsx
@@ -6,6 +6,8 @@ import { collection, getFirestore, onSnapshot, query } from 'firebase/firestore'
 import Strings from './Strings';
 import LanguageContext from './LanguageContext';
 import BackButton from './Components/BackBtn';
+import { FaGreaterThan } from 'react-icons/fa';
+
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -38,23 +40,28 @@ const Regions = () => {
       });
       setRegions(regions);
     });
-  
+
     // Clean up the subscription on unmount
     return () => unsubscribe();
   }, []);
 
   return (
-    <div className="Regions flex flex-col justify-between min-h-screen items-start max-w-md mx-auto">
-      <h1 className="text-3xl px-4 font-bold text-gray-600 mb-4">{Strings[language].regions}</h1>
-      <ul>
-        {regions.map((region) => (
-          <li key={region.id} onClick={() => navigate(`/region/${region.id}`)}>
-            {region.id}
-          </li>
-        ))}
-      </ul>
-      <BackButton />
-    </div>
+    <main className="flex flex-col justify-between min-h-screen w-full">
+      <body className="flex flex-col items-center w-full mx-auto px-8">
+        <h1 className="text-3xl px-4 font-bold text-gray-600 mb-4">{Strings[language].regions}</h1>
+        <ul className="w-full">
+          {regions.map((region) => (
+            <li key={region.id} onClick={() => navigate(`/region/${region.id}`)} className="flex justify-between items-center border p-4 mb-2 rounded shadow">
+              <span>{region.id}</span>
+              <FaGreaterThan />
+            </li>
+          ))}
+        </ul>
+      </body>
+      <footer>
+        <BackButton />
+      </footer>
+    </main>
   );
 };
 
